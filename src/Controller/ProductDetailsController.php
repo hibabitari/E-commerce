090b91e2b09b7c44b128ee8f686b549f443b698e
hibabitari <heba99el@gmail.com>
2026-05-25 03:24:09 +0100

    Dernieres modification

--- src/Controller/ProductDetailsController.php
+++ src/Controller/ProductDetailsController.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace App\Controller;
 
 use App\Entity\Product;
-use App\Service\CartHandler;
+use App\Service\SessionCart;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,13 +10,9 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ProductDetailsController extends AbstractController
 {
     #[Route('/product-details/{id}', name: 'app_product_details')]
-    public function index(
-        Product $product,
-        CartHandler $cartHandler
-    ): Response {
-
-        $cartHandler->addProduct($product);
-
+    public function index(Product $product, SessionCart $cart): Response
+    {
+        $cart->addProduct($product);
         return $this->render('product_details/index.html.twig', [
             'product' => $product,
         ]);
--- src/Service/SessionCart.php
+++ src/Service/SessionCart.php
@@ -1,8 +1,31 @@
 <?php
-
 namespace App\Service;
+
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class SessionCart
 {
+    public function __construct(private RequestStack $requestStack) {}
 
+    public function addProduct($product): void
+    {
+        $session = $this->requestStack->getSession();
+        $cart = $session->get('cart', []);
+        $id = $product->getId();
+        if (isset($cart[$id])) {
+            $cart[$id]['quantity']++;
+        } else {
+            $cart[$id] = [
+                'title' => $product->getTitle(),
+                'price' => $product->getPrice(),
+                'quantity' => 1,
+            ];
+        }
+        $session->set('cart', $cart);
+    }
+
+    public function getCart(): array
+    {
+        return $this->requestStack->getSession()->get('cart', []);
+    }
 }
